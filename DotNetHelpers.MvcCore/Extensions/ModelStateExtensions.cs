ea9233bce74b5097306112ac1674133c67396c9d
Nick Kvizhinadze <nickkvizhinadze@gmail.com>
2020-05-14 22:21:16 +0400

    Added ModelState extensions

---
+++ DotNetHelpers.MvcCore/Extensions/ModelStateExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace DotNetHelpers.MvcCore.Extensions
+{
+    public static class ModelStateExtensions
+    {
+        #region Methods
+        public static void AddErrors(this ModelStateDictionary modelState, IEnumerable<KeyValuePair<string, string>> errors)
+        {
+            foreach (var error in errors)
+                AddError(modelState, error);
+        }
+
+        public static void AddError(this ModelStateDictionary modelState, KeyValuePair<string, string> error)
+        {
+            modelState.AddModelError(error.Key, error.Value);
+        }
+
+        public static string GetErrorsAsString(this ModelStateDictionary modelState)
+        {
+            return string.Join("; ", modelState.Values
+                                        .SelectMany(x => x.Errors)
+                                        .Select(x => x.ErrorMessage));
+        }
+        #endregion
+    }
+}
