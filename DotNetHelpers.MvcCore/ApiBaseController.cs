5ec37380cd664640c90f16183215aa0f424417f2
Nick Kvizhinadze <nickkvizhinadze@gmail.com>
2020-05-14 22:21:24 +0400

    Added api base controller

---
+++ DotNetHelpers.MvcCore/ApiBaseController.cs
@@ -0,0 +1,65 @@
+﻿using System.Net;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using DotNetHelpers.Models;
+using DotNetHelpers.MvcCore.Extensions;
+
+namespace DotNetHelpers.MvcCore
+{
+    [Route("api/[controller]")]
+    public abstract class ApiBaseController : ControllerBase
+    {
+        #region Protected Methods
+        protected ActionResult<T> CustomResult<T>(Result<T> result)
+        {
+            if (!result.Succeeded)
+                return ReturnErrorResult(result);
+
+            return Ok(result.Data);
+        }
+
+        protected IActionResult CustomResult(Result result)
+        {
+            if (!result.Succeeded)
+                return ReturnErrorResult(result);
+
+            return NoContent();
+        }
+        #endregion
+
+        #region Private Methods
+        private ActionResult ReturnErrorResult(Result result)
+        {
+            AddErrors(result.Errors);
+            switch (result.ErrorStatus)
+            {
+                case DotNetHelpers.Enums.ErrorStatus.Server:
+                    return CustomActionResult(HttpStatusCode.InternalServerError);
+                case DotNetHelpers.Enums.ErrorStatus.UnAuthorized:
+                    return CustomActionResult(HttpStatusCode.Unauthorized);
+                case DotNetHelpers.Enums.ErrorStatus.AccessDenied:
+                    return CustomActionResult(HttpStatusCode.Forbidden);
+                default:
+                    return CustomActionResult(HttpStatusCode.BadRequest);
+            }
+        }
+
+        private ActionResult CustomActionResult(HttpStatusCode statusCode)
+        {
+            switch (statusCode)
+            {
+                case HttpStatusCode.BadRequest:
+                    return BadRequest(ModelState);
+                default:
+                    return StatusCode((int)statusCode);
+            }
+        }
+
+        protected void AddErrors(IEnumerable<KeyValuePair<string, string>> errors)
+        {
+            foreach (var error in errors)
+                ModelState.AddError(error);
+        }
+        #endregion
+    }
+}
