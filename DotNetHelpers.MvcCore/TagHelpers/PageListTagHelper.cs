cb1aab5ecb6dd49314b388e37016f31a301e8308
Nick Kvizhinadze <nickkvizhinadze@gmail.com>
2020-08-27 20:04:04 +0400

    Added paging template configuration

--- DotNetHelpers.MvcCore/TagHelpers/PageListTagHelper.cs
+++ DotNetHelpers.MvcCore/TagHelpers/PageListTagHelper.cs
@@ -1,4 +1,6 @@
 ﻿using System.Linq;
+using System.Collections.Generic;
+using DotNetHelpers.MvcCore;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 
@@ -10,6 +12,20 @@ namespace DotNetHelpers.MvcCore.TagHelpers
         private int _startPage;
         private int _pageCount;
         private bool _dotsEntered;
+        private Dictionary<string, string> _classes = new Dictionary<string, string>()
+        {
+            {"ul", "pagination" },
+            {"firstLi", "page-item" },
+            {"prevLi", "page-item" },
+            {"nextLi", "page-item" },
+            {"lastLi", "page-item" },
+            {"firstA", "page-link" },
+            {"prevA", "page-link" },
+            {"nextA", "page-link" },
+            {"lastA", "page-link" },
+            {"li", "page-item" },
+            {"a", "page-link" }
+        };
         #endregion
 
         #region Constructors
@@ -22,6 +38,12 @@ namespace DotNetHelpers.MvcCore.TagHelpers
         public int LastPage { get; set; }
         public int CurrentPage { get; set; }
         public string BaseUrl { get; set; } = string.Empty;
+        public bool ShowFirstAndLast { get; set; } = true;
+        public Dictionary<string, string> Classes { get; set; } = new Dictionary<string, string>();
+        public string FirstElementString { get; set; } = "<i class=\"fas fa-fast-backward\"></i>";
+        public string PrevElementString { get; set; } = "<i class=\"fas fa-step-backward\"></i>";
+        public string NextElementString { get; set; } = "<i class=\"fas fa-step-forward\"></i>";
+        public string LastElementString { get; set; } = "<i class=\"fas fa-fast-forward\"></i>";
         #endregion
 
         #region Override Methods
@@ -29,14 +51,15 @@ namespace DotNetHelpers.MvcCore.TagHelpers
         {
             GenerateVariables();
             output.TagName = "ul";
-            output.Attributes.Add("class", "pagination");
+            output.Attributes.Add("class", Classes["ul"]);
 
-            var first = CreateLiElement(1, "<i class=\"fas fa-fast-backward\"></i>", CurrentPage == 1, false);
-            var previous = CreateLiElement(CurrentPage - 1, "<i class=\"fas fa-step-backward\"></i>", CurrentPage == 1, false);
-            var next = CreateLiElement(CurrentPage + 1, "<i class=\"fas fa-step-forward\"></i>", CurrentPage == LastPage, false);
-            var last = CreateLiElement(LastPage, "<i class=\"fas fa-fast-forward\"></i>", CurrentPage == LastPage, false);
+            var first = CreateLiElement(1, PrevElementString, CurrentPage == 1, false, Classes["firstLi"], Classes["firstA"]);
+            var previous = CreateLiElement(CurrentPage - 1, PrevElementString, CurrentPage == 1, false, Classes["prevLi"], Classes["prevA"]);
+            var next = CreateLiElement(CurrentPage + 1, NextElementString, CurrentPage == LastPage, false, Classes["nextLi"], Classes["nextA"]);
+            var last = CreateLiElement(LastPage, LastElementString, CurrentPage == LastPage, false, Classes["lastLi"], Classes["lastA"]);
 
-            output.Content.AppendHtml(first);
+            if (ShowFirstAndLast)
+                output.Content.AppendHtml(first);
             output.Content.AppendHtml(previous);
 
             foreach (var i in Enumerable.Range(_startPage, _pageCount))
@@ -46,44 +69,45 @@ namespace DotNetHelpers.MvcCore.TagHelpers
                     if (i - _startPage > 6 && !_dotsEntered)
                     {
                         _dotsEntered = true;
-                        var li = CreateLiElement(0, "...", true, false);
+                        var li = CreateLiElement(0, "...", true, false, Classes["li"], Classes["a"]);
                         output.Content.AppendHtml(li);
                     }
                 }
                 else
                 {
-                    var li = CreateLiElement(i, i.ToString(), false, CurrentPage == i);
+                    var li = CreateLiElement(i, i.ToString(), false, CurrentPage == i, Classes["li"], Classes["a"]);
                     output.Content.AppendHtml(li);
                 }
             }
 
             output.Content.AppendHtml(next);
-            output.Content.AppendHtml(last);
+            if (ShowFirstAndLast)
+                output.Content.AppendHtml(last);
         }
         #endregion
 
         #region Private Methods
-        private TagBuilder CreateLiElement(int page, string anchoreText, bool isDisabled, bool isActive)
+        private TagBuilder CreateLiElement(int page, string anchoreText, bool isDisabled, bool isActive, string liClass, string aClass)
         {
             var result = new TagBuilder("li");
-            result.AddCssClass("page-item");
+            result.AddCssClass(liClass);
             if (isDisabled)
                 result.AddCssClass("disabled");
 
             if (isActive)
                 result.AddCssClass("active");
 
-            var a = CreateAnchorElement(page, anchoreText);
+            var a = CreateAnchorElement(page, anchoreText, aClass);
 
             result.InnerHtml.AppendHtml(a);
             return result;
         }
 
-        private TagBuilder CreateAnchorElement(int page, string text)
+        private TagBuilder CreateAnchorElement(int page, string text, string aClass)
         {
             var a = new TagBuilder("a");
 
-            a.AddCssClass("page-link");
+            a.AddCssClass(aClass);
 
             var url = BaseUrl + (BaseUrl.Contains("?") ? $"&" : "?") + $"page={page}";
             a.Attributes.Add("href", url);
@@ -95,6 +119,10 @@ namespace DotNetHelpers.MvcCore.TagHelpers
 
         private void GenerateVariables()
         {
+            foreach (var item in _classes)
+                if (!Classes.ContainsKey(item.Key))
+                    Classes.Add(item.Key, item.Value);
+
             var numberOfShownPages = Config.NumberOfShownPages;
             if (LastPage < numberOfShownPages)
             {
