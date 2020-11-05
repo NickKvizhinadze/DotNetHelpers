ffce1ec7bee628863a80551b6d6d1eafad11cb78
Nick Kvizhinadze <nickkvizhinadze@gmail.com>
2020-11-05 15:53:14 +0400

    Added parametername to ThrowIfNull method

--- DotNetHelpers/Extentions/ObjectExtentions.cs
+++ DotNetHelpers/Extentions/ObjectExtentions.cs
@@ -2,16 +2,20 @@
 
 namespace DotNetHelpers.Extentions
 {
+    /// <summary>
+    /// Object Extentions
+    /// </summary>
     public static class ObjectExtentions
     {
         /// <summary>
         /// Throws Argmunet null exception if object is null
         /// </summary>
         /// <param name="source">Object to check </param>
-        public static void ThrowIfNull(this object source)
+        /// <param name="parameterName">Name of parameter</param>
+        public static void ThrowIfNull(this object source, string parameterName)
         {
             if (source == null)
-                throw new ArgumentNullException();
+                throw new ArgumentNullException(parameterName);
         }
     }
 }
